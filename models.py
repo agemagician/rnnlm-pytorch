203fec0c47091f4d1bf489264548147a60d92818
Ahmed Elnaggar <amit.najjar@gmail.com>
2020-02-25 21:53:25 +0100

    Update models.py

--- models.py
+++ models.py
@@ -220,11 +220,9 @@ class RNNModel(nn.Module):
         self.drop = nn.Dropout(prm["dropout"])
         self.emb_dim = 0
         if self.prm["wo_tok"]:
-            #self.emb_dim = prm["char_hid"]
-            self.emb_dim = prm["tok_hid"]
-        elif self.prm["wo_char"]:
-            #self.emb_dim = prm["tok_emb"]
             self.emb_dim = prm["char_hid"]
+        elif self.prm["wo_char"]:
+            self.emb_dim = prm["tok_emb"]
         elif prm["wo_tok"] and prm["wo_char"]:
             assert(False)
         else:
@@ -291,14 +289,12 @@ class RNNModel(nn.Module):
         ----------
         emb: The shape of this value depends on self.prm["wo_tok"] and self.prm["wo_char"]
         """
-        #if self.prm["wo_tok"]:
-        if self.prm["wo_char"]:
+        if self.prm["wo_tok"]:
             # emb: [seq_len*nbatch, char_hid]
             emb = self.drop(self.char_encoder(input["char"]))
             # emb: [seq_len, nbatch, char_hid]
             emb = emb.reshape(input["word"].shape[0], input["word"].shape[1], -1)
-        #elif self.prm["wo_char"]:
-        elif self.prm["wo_tok"]:
+        elif self.prm["wo_char"]:
             # emb: [seq_len, nbatch, tok_emb]
             emb = self.drop(self.word_encoder(input["word"]))
         elif self.prm["wo_tok"] and self.prm["wo_char"]:
